f34f2a73b3598f0535a21f0954ff704dec3f0202
Nicolas1st <nicolashereigo@gmail.com>
2023-07-18 01:03:18 +0300

    New impl

--- errors.go
+++ errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrRegistrationStopped = errors.New("operations can no longer be registered")
 	ErrNothingToRegister   = errors.New("the op count is already 0, nothing to unregister")
+	ErrNegativeOpsCount    = errors.New("negative ops count")
 )
--- shutdown.go
+++ shutdown.go
@@ -1,58 +1,43 @@
 package shutdown
 
 import (
-	"sync"
 	"sync/atomic"
 )
 
 type GracefulShutter struct {
-	opsCount            *int64
-	stoppedRegistering  bool
-	mu                  *sync.RWMutex
-	finishedWorkingChan chan struct{}
+	opsCount *int64
+	stopChan chan struct{}
 }
 
 func NewConcurrentOpsRegister() *GracefulShutter {
 	return &GracefulShutter{
-		opsCount:            new(int64),
-		stoppedRegistering:  false,
-		mu:                  &sync.RWMutex{},
-		finishedWorkingChan: make(chan struct{}),
+		opsCount: new(int64),
+		stopChan: make(chan struct{}),
 	}
 }
 
-func (s *GracefulShutter) RegisterOp() error {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if s.stoppedRegistering {
+func (s *GracefulShutter) RegOp() error {
+	select {
+	case <-s.stopChan:
 		return ErrRegistrationStopped
+	default:
+		atomic.AddInt64(s.opsCount, 1)
+		return nil
+	}
+}
+
+func (s *GracefulShutter) UnregOp() error {
+	if atomic.LoadInt64(s.opsCount) == 0 {
+		return ErrNothingToRegister
 	}
 
-	atomic.AddInt64(s.opsCount, 1)
+	if newOpsCount := atomic.AddInt64(s.opsCount, -1); newOpsCount < 0 {
+		return ErrNegativeOpsCount
+	}
 
 	return nil
 }
 
-func (s *GracefulShutter) UnregisterOp() error {
-	if atomic.LoadInt64(s.opsCount) != 0 {
-		atomic.AddInt64(s.opsCount, -1)
-		return nil
-	}
-
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	if s.stoppedRegistering && atomic.LoadInt64(s.opsCount) == 0 {
-		s.finishedWorkingChan <- struct{}{}
-		return nil
-	} else {
-		return ErrNothingToRegister
-	}
-}
-
 func (s *GracefulShutter) StopRegistering() {
-	s.mu.Lock()
-	s.stoppedRegistering = true
-	s.mu.Unlock()
-
-	<-s.finishedWorkingChan
+	close(s.stopChan)
 }
