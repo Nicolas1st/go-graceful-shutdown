aad14e6a502e31723e55e062db3e8096c6a5849f
Nicolas1st <nicolashereigo@gmail.com>
2023-07-18 00:23:46 +0300

    Change

--- shutdown.go
+++ shutdown.go
@@ -2,6 +2,7 @@ package shutdown
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 type GracefulShutter struct {
@@ -18,4 +19,40 @@ func NewConcurrentOpsRegister() *GracefulShutter {
 		mu:                  &sync.RWMutex{},
 		finishedWorkingChan: make(chan struct{}),
 	}
+}
+
+func (s *GracefulShutter) RegisterOp() error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if s.stoppedRegistering {
+		return ErrRegistrationStopped
+	}
+
+	atomic.AddInt64(s.opsCount, 1)
+
+	return nil
+}
+
+func (s *GracefulShutter) UnregisterOp() error {
+	if atomic.LoadInt64(s.opsCount) != 0 {
+		atomic.AddInt64(s.opsCount, -1)
+		return nil
+	}
+
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if s.stoppedRegistering && atomic.LoadInt64(s.opsCount) == 0 {
+		s.finishedWorkingChan <- struct{}{}
+		return nil
+	} else {
+		return ErrNothingToRegister
+	}
+}
+
+func (s *GracefulShutter) StopRegistering() {
+	s.mu.Lock()
+	s.stoppedRegistering = true
+	s.mu.Unlock()
+
+	<-s.finishedWorkingChan
 }
